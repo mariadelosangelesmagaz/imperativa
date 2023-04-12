705ef0b331f125693c67e5759926261735ecfcb1
mariadelosangeles <mariadelosangelesmagaz@gmail.com>
2023-04-11 22:27:18 -0300

    primer commit

---
+++ practicas/clase5S.js
@@ -0,0 +1,37 @@
+function test1(x, y) {
+    return y - x
+}
+
+test1 (10, 40)
+
+function test2 (x,y){
+    return x * 2
+    console.log (x)
+    return x/2
+}
+
+function pulacenti (pul){
+    return pul * 2,54
+}
+console.log(pulacenti)
+
+function string (frase){
+return "https."+ frase+".com"
+}
+console.log (string("raulito"))
+
+function string (frase1){
+    return `"¡"+ frase1+"!"`
+    }
+    console.log (string("raulito"))
+    
+function perros (años){
+    return años *7
+}
+function sueldo (monto){
+    return monto / 40
+}
+
+function imc (kilos, altura){
+    return kilos / (altura)^2
+}
